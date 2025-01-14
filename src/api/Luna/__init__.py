203fdac8a0bf83743050d46fc2588a097b5c8ff8
Suggestion <84503411+hcl595@users.noreply.github.com>
2025-01-14 23:09:23 +0800

    build Luna(only for test)

---
+++ src/api/Luna/__init__.py
@@ -0,0 +1,6 @@
+
+__version__ = "0.0.1"
+
+from .extractor import extract_codeblock as extract_codeblock
+from .extractor import write_pyFile as write_pyFile
+from .kit import uniRequest as uniRequest
--- src/api/api.py
+++ src/api/api.py
@@ -1,13 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, Form
+from fastapi.responses import StreamingResponse
+from fastapi.requests import Request
+from fastapi.encoders import jsonable_encoder
+
+#Original
+# from setup import setup
+# setup()
+# from config import Prompt, Settings
+# from data import History, Models, Widgets, Sessions, APIs
+from Luna.extractor import write_pyFile
+from Luna.kit import uniRequest
+# from models import *
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"status": "200"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+# @app.post("/stream")
+# async def stream_response():
+#     try:
+#         type = Models.get(Models.id == Sessions.get(Sessions.id == InputSession).model_id).type
+#         Model_response = request_OpenAI(SessionID=InputSession, Userinput=InputInfo, stream=True)
+#         async for r in Model_response:
+#             yield StreamingResponse(r[0])
+#     except:
+#         r = request_Json(SessionID=InputSession, Userinput=InputInfo,)
+#         yield StreamingResponse(r[0])
+
+@app.post("/request_models_stream")
+async def request_models_stream(
+    InputAPIType:str = Form(),
+    InputAPIKey:str = Form(),
+    InputModelName:str = Form(),
+    InputUrl:str = Form(),
+    InputQuestion:str = Form(),
+    InputHistorys:str = Form()
+    ):
+    if not InputAPIType or not InputAPIKey or not InputUrl or not InputQuestion:
+        raise HTTPException(status_code=400, detail="Missing userinput or modelinput")
+    if not InputHistorys:
+        InputHistorys = []
+    else:
+        InputHistorys = jsonable_encoder(InputHistorys)
+    async def stream():
+        async for response in uniRequest(
+            API=InputAPIType,
+            APIKey=InputAPIKey,
+            modelName=InputModelName,
+            requestURL=InputUrl,
+            userInput=InputQuestion,
+            historys=InputHistorys,
+            stream=True):
+            yield response
+    return StreamingResponse(stream(), media_type="text/plain")
